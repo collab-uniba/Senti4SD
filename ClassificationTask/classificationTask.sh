0b3655bb139a1a286c7d7597d73002a95f56ff5b
Federico Maiorano <maior.federico@gmail.com>
2017-12-21 10:56:40 +0100

    Update classificationTask.sh

--- ClassificationTask/classificationTask.sh
+++ ClassificationTask/classificationTask.sh
@@ -12,10 +12,13 @@ then
 fi
 
 #-F A: all features to be considered
-#-i file_name.csv: a file containg a document for every line
+#-i file_name: a file containg a document for every line
 #-W cbow600.bin: DSM to be loaded
 #-oc file_name.csv: output dataset containg the features extracted
 #-vd numeric: vectors size (for cbow600.bin the size is 600)
+#-L: if present corpus have a label column [optional]
+#-ul file_name: unigram's list to use for feature extraction. If not present default Senti4SD unigram's list will be used [optional]
+#-bl file_name: bigram's list to use for feature extraction. If not present default Senti4SD bigram's list will be used [optional]
 
 java -jar Senti4SD.jar -F A -i $1 -W dsm.bin -oc extractedFeatures.csv -vd 600
 #classificate using as model "modelLiblinear.Rda", builded with "LiblineaR", 
