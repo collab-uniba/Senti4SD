8a3467e9e8dbaa0af9343862aadc83a56a4668bc
Federico Maiorano <maior.federico@gmail.com>
2019-06-24 13:13:49 +0200

    Update requirements.sh
    Removed backticks around the two ```which```.

--- ClassificationTask/requirements.sh
+++ ClassificationTask/requirements.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [ -n `which java` ]; then
+if [ -n $(which java) ]; then
 	echo "Java is installed"
-	if [ -n `which R` ]; then
+	if [ -n $(which R) ]; then
                 echo "R is installed"
 		#this will install R library caret and LiblineaR if not already installed
 		Rscript requirements.R
